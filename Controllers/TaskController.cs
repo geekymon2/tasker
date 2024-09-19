cfbc4d11c298c14e04f1374ae1958df378166f87
geekymon2 <geekymon2@gmail.com>
2024-09-19 18:40:55 +1000

    changes

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,4 @@
+class TaskController : Microsoft.AspNetCore.Mvc.Controller
+{
+    public TaskController() { }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,7 @@
+class Task
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public bool IsComplete { get; set; }    
+}
