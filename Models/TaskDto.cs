c53e443cc57221864e5542f20894acd07f84d915
geekymon2 <geekymon2@gmail.com>
2024-09-25 16:26:44 +1000

    Update TaskDto.cs

--- Models/TaskDto.cs
+++ Models/TaskDto.cs
@@ -5,4 +5,5 @@ public class TaskDto
     public required string Id { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
+    public required string IsComplete { get; set; }
 }   
