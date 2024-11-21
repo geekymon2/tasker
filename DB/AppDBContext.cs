c9130c12b4e32fc97247cf2116a177fcfe9594e1
geekymon2 <geekymon2@gmail.com>
2024-11-21 12:06:51 +1100

    updated settings to load from environment variables

--- DB/AppDBContext.cs
+++ DB/AppDBContext.cs
@@ -1,18 +1,23 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace GeekyMon2.Tasker.DB;
 
-public class AppDBContext(DbContextOptions<AppDBContext> options, IConfiguration config) : DbContext(options)
+public class AppDBContext(DbContextOptions<AppDBContext> options, IConfiguration config, ILogger<AppDBContext> logger) : DbContext(options)
 {
     private readonly IConfiguration _config = config;
+    private readonly ILogger<AppDBContext> _logger = logger;
     public DbSet<Entities.Task> Tasks { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        string endpoint = _config.GetValue<string>("AppConfig:CosmosEndpoint") ?? "https://localhost:8081";
-        string key = _config.GetValue<string>("AppConfig:CosmosKey") ?? "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-        string db = _config.GetValue<string>("AppConfig:CosmosDatabase") ?? "tasker-db-dev";
+        _logger.LogInformation("Configuring DB Context");
+ 
+        string endpoint = _config.GetValue<string>("AppConfig:CosmosEndpoint") ?? "";
+        string key = _config.GetValue<string>("AppConfig:CosmosKey") ?? "";
+        string db = _config.GetValue<string>("AppConfig:CosmosDatabase") ?? "";
+
+        _logger.LogInformation("DB endpoint: {endpoint}", endpoint);
+        _logger.LogInformation("DB Name: {db}", db);
 
         optionsBuilder.UseCosmos(
             accountEndpoint: endpoint,
@@ -21,10 +26,11 @@ public class AppDBContext(DbContextOptions<AppDBContext> options, IConfiguration
         );
     }
 
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         string container = _config.GetValue<string>("AppConfig:CosmosContainer") ?? "tasker-container-dev";
+
+        _logger.LogInformation("Container Name: {container}", container);
 
         modelBuilder.Entity<Entities.Task>().Property(c => c.status).HasConversion<string>();
         modelBuilder.Entity<Entities.Task>().ToContainer(container).HasPartitionKey(x => x.Id);
